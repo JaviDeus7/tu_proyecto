5a636f47bfb9494f28681b1c1258c8bd54e547f5
JavierS <100806455+JaviDeus7@users.noreply.github.com>
2023-10-29 21:37:17 +0100

    Delete boda directory

--- boda/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BodaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'boda'
--- boda/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def bienvenida(request):
-    return render(request, 'boda/bienvenida.html')
-
-def pagina_principal(request):
-    return render(request, 'boda/pagina_principal.html')
