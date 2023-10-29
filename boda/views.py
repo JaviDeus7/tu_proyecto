f8e72a09123bbd5421bffbecd29e62c6faec6e80
JavierS <100806455+JaviDeus7@users.noreply.github.com>
2023-10-29 21:38:43 +0100

    Final1

---
+++ boda/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bienvenida(request):
+    return render(request, 'boda/bienvenida.html')
+
+def pagina_principal(request):
+    return render(request, 'boda/pagina_principal.html')
+
+def ceremonia(request):
+    return render(request, 'boda/ceremonia.html')
+
+def celebracion(request):
+    return render(request, 'boda/celebracion.html')
+
+def hashtag(request):
+    return render(request, 'boda/hashtag.html')
+
+def musica(request):
+    return render(request, 'boda/musica.html')
+
+def albumFotos(request):
+    return render(request, 'boda/albumFotos.html')
+
+def listaBoda(request):
+    return render(request, 'boda/listaBoda.html')
+
+def confirmaAsistencia(request):
+    return render(request, 'boda/confirmaAsistencia.html')
---
+++ tu_proyecto/urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for tu_proyecto project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from boda import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.bienvenida, name='bienvenida'),
+    path('Principal/', views.pagina_principal, name='pagina_principal'),
+    path('Ceremonia/', views.ceremonia, name='ceremonia'),
+    path('Celebracion/', views.celebracion, name='celebracion'),
+    path('Hashtag/', views.hashtag, name='hashtag'),
+    path('Musica/', views.musica, name='musica'),
+    path('AlbumFotos/', views.albumFotos, name='albumFotos'),
+    path('ListaBoda/', views.listaBoda, name='listaBoda'),
+    path('ConfirmaAsistencia/', views.confirmaAsistencia, name='confirmaAsistencia'),
+]
